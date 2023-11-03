a85f84bbdbf090e0fecdffc2eed1d675f4965837
Ertan Donmez <ertandonmez1@gmail.com>
2023-11-03 11:47:43 +0200

    initial commit

---
+++ 02.JS Arrays & Strings/02.JS-Front-End-JS Arrays-and-Strings-MoreExercises/03-thePyramidofKingDjoser.js
@@ -0,0 +1,48 @@
+function solve(base, increacment) {
+    let stone = 0;
+    let marble = 0;
+    let lazuli = 0;
+    let gold = 0 ;
+    let width = base;
+    let length = base;
+    let height = increacment;
+    let layer = 0;
+
+    while (width > 0) {
+        layer += 1;
+        let size = width * length;
+        let innner = ((width - 2) * (length - 2)) ;
+        let outer = size - innner;
+        if (width === 1) {
+            outer = 1
+            innner = 0
+            gold += outer * height
+        } else if (width === 2) {
+            outer = 4
+            innner = 0
+            gold += outer * height
+        } else if (layer % 5 === 0) {
+            lazuli += outer * height
+        } else {
+            marble += outer * height;
+        }
+        stone += innner * height;
+       
+        width -= 2;
+        length -= 2;
+    }
+    console.log(`Stone required: ${Math.ceil(stone)}`);
+    console.log(`Marble required: ${Math.ceil(marble)}`);
+    console.log(`Lapis Lazuli required: ${Math.ceil(lazuli)}`);
+    console.log(`Gold required: ${Math.ceil(gold)}`);
+    console.log(`Final pyramid height: ${Math.floor(layer * height)}`);
+}
+
+solve(11, 1)
+console.log();
+solve(11, 0.75)
+console.log();
+solve(12, 1)
+console.log();
+solve(23, 0.5)
+console.log();
---
+++ 03.Functions and Statements/03.JS-Front-End-Functions-and-Statements-Exercise/01-smallestofThreeNumbers.js
@@ -0,0 +1,11 @@
+function findTheSmallest (num1, num2, num3) {
+    // let numsArr = [num1, num2, num3]
+    let smallest = Math.min(num1, num2, num3)
+    console.log(smallest)
+        
+}
+
+findTheSmallest (2, 5, 3)
+findTheSmallest (600, 342, 123)
+findTheSmallest (25, 21, 4)
+findTheSmallest (2, 2, 2)
