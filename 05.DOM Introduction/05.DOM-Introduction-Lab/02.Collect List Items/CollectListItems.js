65ec277865f2a7d9f5034223d3479db06286ffe7
Ertan Donmez <ertandonmez1@gmail.com>
2023-11-18 11:46:15 +0200

    initial commit

--- 05.DOM Introduction/05.DOM-Introduction-Lab/02.Collect List Items/CollectListItems.js
+++ 05.DOM Introduction/05.DOM-Introduction-Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,11 @@
 function extractText() {
-    // TODO
+    const listItems = Array.from(document.querySelectorAll('li'))
+    // console.log (listItems)
+    let textResult = ''
+    for (let item of listItems) {
+        textResult += item.textContent + '\n';
+    }
+    // const text = listItems.map((item) => item.textContent).join('\n');
+    document.querySelector('textarea').value = textResult
 }
+
--- 05.DOM Introduction/05.DOM-Introduction-Lab/03.Sum Numbers/SumNumbers.js
+++ 05.DOM Introduction/05.DOM-Introduction-Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,8 @@
 function calc() {
     // TODO: sum = num1 + num2
+    let num1 = document.getElementById('num1').value
+    let num2 = document.getElementById('num2').value
+    let sum = parseInt(num1) + parseInt(num2)
+
+    document.getElementById('sum').value = sum 
 }
