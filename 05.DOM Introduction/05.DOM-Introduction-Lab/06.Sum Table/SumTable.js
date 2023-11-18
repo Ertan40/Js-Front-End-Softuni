903eecbbf826b2ab4a8d7672ff3b592295dcaf94
Ertan Donmez <ertandonmez1@gmail.com>
2023-11-18 14:52:25 +0200

    initial commit

--- 05.DOM Introduction/05.DOM-Introduction-Lab/06.Sum Table/SumTable.js
+++ 05.DOM Introduction/05.DOM-Introduction-Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,8 @@
 function sumTable() {
-
+    let costs = Array.from(document.querySelectorAll('td:nth-child(even)'))
+    let total = costs.reduce((acc, curr) => {
+        return acc + Number(curr.textContent);
+}, 0);
+    document.getElementById('sum').textContent = total
+    // console.log(total);
 }
--- 05.DOM Introduction/05.DOM-Introduction-Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ 05.DOM Introduction/05.DOM-Introduction-Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,8 @@
 function extract(content) {
+    let text = document.getElementById(content).textContent
+    console.log(text);
+    let pattern = /\([\w ]+\)/g
+    let result = text.match(pattern)
 
+    return result.join('; ')
 }
