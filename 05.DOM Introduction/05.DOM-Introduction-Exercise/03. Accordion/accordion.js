95494c2775bb706943062d1c20b68902039b6fc6
Ertan Donmez <ertandonmez1@gmail.com>
2023-11-19 12:35:11 +0200

    initial commit

--- 05.DOM Introduction/05.DOM-Introduction-Exercise/03. Accordion/accordion.js
+++ 05.DOM Introduction/05.DOM-Introduction-Exercise/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+
+    let button = document.getElementsByClassName("button")[0];
+    let displayText = document.getElementById("extra")
+    // let displayText = document.getElementById("extra").style.display
+    
+    if (button.textContent === 'More') {
+        displayText.style.display = "block"
+        button.textContent = 'Less'
+    } else {
+        displayText.style.display = "none"
+        button.textContent = 'More'
+    }
 }
