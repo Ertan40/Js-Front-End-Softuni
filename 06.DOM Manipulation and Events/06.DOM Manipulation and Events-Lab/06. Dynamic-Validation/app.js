82f067fb9bf1196bd9d36051895ce2da2daed94d
Ertan Donmez <ertandonmez1@gmail.com>
2023-11-22 12:14:12 +0200

    initial commit

--- 06.DOM Manipulation and Events/06.DOM Manipulation and Events-Lab/06. Dynamic-Validation/app.js
+++ 06.DOM Manipulation and Events/06.DOM Manipulation and Events-Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    const input = document.getElementById("email");
+    let regexp = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-zA-Z]{2,}$/g;
+    // let regexp = /[a-zA-Z]{1,}@[a-zA-Z]{1,}.[a-zA-Z]{1,}/g
+    
+    input.addEventListener('change', () => {
+        let text = input.value;
+
+        if (!regexp.test(text)) {
+            input.classList.add("error")
+        } else {
+            input.classList.remove("error")
+        }
+    })
 }
+   
