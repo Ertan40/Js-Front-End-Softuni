bec1ff88c51d62c7be27422a0d2b70542fe119a0
Ertan Donmez <ertandonmez1@gmail.com>
2023-11-28 09:29:12 +0200

    initial commit

---
+++ 07.HTTP and REST - Lab/02. GitHub-Repos/scripts/app.js
@@ -0,0 +1,12 @@
+function loadRepos() {
+   fetch("https://api.github.com/users/testnakov/repos")
+      .then((res) => res.text())
+      .then(data => {
+          document.getElementById("res").textContent = data
+      })
+      .catch(error => console.error('Error:', error));
+      // .catch(() => console.log(error!))
+}
+
+
+
