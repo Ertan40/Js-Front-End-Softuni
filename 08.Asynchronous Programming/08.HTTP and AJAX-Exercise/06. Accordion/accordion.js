f7083d9805dbe202c28eaf362416f3783535c8c0
Ertan Donmez <ertandonmez1@gmail.com>
2023-12-10 12:22:02 +0200

    update

--- 08.Asynchronous Programming/08.HTTP and AJAX-Exercise/06. Accordion/accordion.js
+++ 08.Asynchronous Programming/08.HTTP and AJAX-Exercise/06. Accordion/accordion.js
@@ -1,3 +1,48 @@
 function solution() {
-    //TODO .....
+    const BASE_URL = "http://localhost:3030/jsonstore/advanced/articles/"
+    const main = document.querySelector("#main");
+    main.innerHTML = "";
+
+    function loadArticles(data) {
+        Object.values(data).forEach((article) => {
+            const div = document.createElement("div");
+            div.className = "accordion";
+            div.innerHTML = `
+                <div class="head">
+                    <span>${article.title}</span>
+                    <button class="button" id=${article._id}>More</button>
+                </div>
+                <div class="extra">
+                </div>
+                `;
+            main.appendChild(div);
+            const button = div.querySelector(".button");
+            const extra = div.querySelector(".extra");
+
+            function getMore(e) {
+                extra.innerHTML = "";
+                fetch(`${BASE_URL}details/${e.target.id}`)
+                .then(response => response.json())
+                .then(data => extra.innerHTML = `<p>${data.content}</p>`)
+                .catch(error => console.log(error))
+                if (button.textContent === "More") {
+                    button.textContent = "Less";
+                    extra.style.display = "block";
+                } else {
+                    button.textContent = "More";
+                    extra.style.display = "none";
+                }
+            }
+            button.addEventListener("click", getMore);
+        });
+    }
+    
+    fetch(`${BASE_URL}list`)
+    .then(response => response.json())
+    .then(data => loadArticles(data))
+    .catch(error => console.log(error))
 }
+  
+solution();
+
+
